44d39e5c9e3a14e370ec8e07636f847c65f08a2a
Karan Yede <115563014+karanyede@users.noreply.github.com>
2025-05-27 21:41:58 +0530

    Update TwoSum.java

--- Hashmap/Easy/TwoSum.java
+++ Hashmap/Easy/TwoSum.java
@@ -1,28 +1,26 @@
-package TwoPointer.Easy;
-
-public class TwoSum{
-    public static int[] twoSum(int[] nums, int target){
-        int left = 0, right = nums.length-1;
-        while(left<=right){
-            int sum = nums[left] + nums[right];
-            if(sum == target){
-                return new int[]{left,right};
+class Solution {
+    //Most efficient solution for this problem
+    public int[] twoSum(int[] nums, int target) {
+        //We are creating a Hashmap with 
+        //key as difference beween target elemt and the index element
+        //value as current index
+        Map<Integer,Integer> map = new HashMap<>();
+        // Loop through the array of numbers.
+        for(int i=0;i<nums.length;i++){
+            // Check if the current number exists as a key in the HashMap.
+            // If it does, that means we have already encountered a number earlier in the array
+            // such that its complement (target - number) equals the current number.
+            Integer index = map.get(nums[i]);
+            // If we find such a number, return the indices of the two numbers.
+            if(index != null){
+                return new int[]{index,i};
             }
-            else if (sum < target){
-                left++;
-            }
-            else{
-                right--;
-            }
+            // Otherwise, calculate the complement (target - current number) and store it in the HashMap.
+            // The key is the complement, and the value is the index of the current number.
+            // This allows us to quickly check later if the complement exists in the array.
+            map.put(target-nums[i],i);
         }
+        // If no solution is found, return
         return new int[]{-1,-1};
-    }
-
-    public static void main(String args[]){
-        int nums[]={1,2,3,4,5,6};
-        int target = 3;
-        int result[] = twoSum(nums,target);
-        System.out.println(result[0] + " " + result[1]);
-        // Output: 0 1 (since nums[0] + nums[1] = 1 + 2 = 3)
     }
 }
